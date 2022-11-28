9449eb9e45d3ecc0fb828383680967552381a170
Emily.Garcia <emilikimberly1@gmail.com>
2022-11-28 16:48:31 -0500

    styling

--- app/EventDate.tsx
+++ app/EventDate.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function EventDate() {
   return (
-    <div className='h-full w-full bg-brick bg-opacity-50 px-20 flex justify-center items-end'>
-      <div className='flex flex-col items-center justify-evenly text-whitesmoke p-10 pb-20 bg-brick bg-opacity-95 text-center'>
+    <div className='h-full w-full bg-brick bg-opacity-40 px-20 flex justify-center items-end'>
+      <div className='flex flex-col items-center justify-evenly text-whitesmoke p-10 pb-20 bg-brick bg-opacity-80 text-center'>
         <p className='text-7xl m-10'>Saturday, October 15th</p>
         <p className='text-5xl m-10 mb-20'>at <span className='text-6xl'>4:00PM</span></p>
         <p className="text-6xl font-bold mt-10">144 Crowell St.</p>
