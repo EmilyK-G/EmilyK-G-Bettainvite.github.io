ee3106f4324ccbe71c9a44eed3d5cde931ca3652
Emily.Garcia <emilikimberly1@gmail.com>
2022-11-29 18:00:00 -0500

    styling

--- app/Event.tsx
+++ app/Event.tsx
@@ -1,8 +1,5 @@
 import Link from 'next/link';
 
-// import Image from 'next/image';
-// import woman from '../public/bettainvite_woman.jpg';
-
 function Event() {
   return (
     <div className="flex flex-col items-center justify-between h-full text-whitesmoke bg-midnight bg-opacity-40 text-center">
@@ -15,13 +12,6 @@ function Event() {
           Details
         </Link>
       </div>
-      {/* <div className='flex-1 relative'>
-        <Image 
-          src={woman}
-          alt='woman picture'
-          className='object-cover h-full z-0'/>
-        <div className='absolute bg-midnight bg-opacity-40 z-30 flex-1'></div>
-      </div> */}
       <div className='flex flex-col items-end justify-center p-10 px-32 bg-midnight w-full'>
         <p className='text-6xl font-thin m-10'> <small className='text-5xl'>Hosted by</small> Emily Kimberly</p>
       </div>
