0ffa30f8cc582459cec2418870fd173c24240e09
Emily.Garcia <emilikimberly1@gmail.com>
2022-12-13 08:17:53 -0500

    date correction

--- app/EventDate.tsx
+++ app/EventDate.tsx
@@ -7,7 +7,7 @@ function EventDate() {
         <h1 className='text-9xl m-10'>Lugar y Fecha:</h1>
         <div>
           <p className='text-8xl m-10 leading-snug'>Domingo 18 de Diciembre</p>
-          <p className='text-5xl m-30 font-mono'>desde la <span className='text-6xl'>01:00PM</span></p>
+          <p className='text-5xl m-30 font-mono'>de <span className='text-6xl'>10:00AM</span> a <span className='text-6xl'>05:00PM</span></p>
         </div>
         <p className="text-5xl font-mono font-thin">Lorenzo de Garycoa y Venezuela</p>
       </div>
