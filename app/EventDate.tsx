ab279a89dfe37065c95ae802d97e6f638a9c69e9
Emily.Garcia <emilikimberly1@gmail.com>
2022-12-30 19:19:49 -0500

    date

--- app/EventDate.tsx
+++ app/EventDate.tsx
@@ -7,7 +7,7 @@ function EventDate() {
         <h1 className='text-9xl m-10'>Date & Time</h1>
         <div>
           <p className='text-8xl m-10 leading-snug'>Saturday, December 31st</p>
-          <p className='text-5xl m-30 font-mono'><span className='text-6xl'>5:00PM</span> to <span className='text-6xl'>8:00PM</span></p>
+          <p className='text-5xl m-30 font-mono'><span className='text-6xl'>8:00PM</span> to <span className='text-6xl'>10:00PM</span></p>
         </div>
         <p className="text-5xl font-mono font-thin">144 Crowell St. Hempstead, NY 11550</p>
       </div>
