1c31851b4f93acb79d1af4c8da24527ab1a00e8f
Emily.Garcia <emilikimberly1@gmail.com>
2023-05-30 17:03:30 -0400

    Homepage links

--- app/websitesList.tsx
+++ app/websitesList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface eventProps {
     event: {eventId:string, name:string, theme:string, bgr: string},
@@ -7,18 +8,19 @@ interface eventProps {
 
 const WebsitesList = ({event}:eventProps) => {
   return (
-    <div 
-        className="flex flex-col items-center justify-center w-full h-44 my-1 border-spacing-10 border-y-2 border-lightgray relative z-20">
-            <Image 
-                src={`/backgrounds/`+ event.bgr} 
-                alt='image'
-                fill={true}
-                sizes="fill"
-                className='object-cover opacity-90'/>
-                <div className="flex items-center justify-end h-full w-full mr-3 bg-whitesmoke bg-opacity-5 relative z-10 hover:bg-opacity-0 ">
-                    <p className='z-20'>{event.name}</p>
-                </div>
-        </div>
+            <Link 
+              href={"/" + event.name + "/" + event.eventId} 
+              className="flex flex-col items-center justify-center w-full h-44 my-1 border-spacing-10 border-y-2 border-lightgray relative z-20">
+                <Image 
+                    src={`/backgrounds/`+ event.bgr}
+                    alt='image'
+                    fill={true}
+                    sizes="fill"
+                    className='object-cover opacity-90'/>
+                    <div className="flex items-center justify-end h-full w-full mr-3 bg-whitesmoke bg-opacity-5 relative z-10 hover:bg-opacity-0 hover:text-whitesmoke">
+                        <p className='z-20'>{event.name}</p>
+                    </div>
+            </Link>
   )
 }
 
