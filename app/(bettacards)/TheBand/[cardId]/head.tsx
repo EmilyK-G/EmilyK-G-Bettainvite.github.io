e58e29bc72feed6de3d976d305a6d95d7880a6ed
Emily.Garcia <emilikimberly1@gmail.com>
2023-06-26 12:50:47 -0400

    meta tag

--- app/(bettacards)/TheBand/[cardId]/head.tsx
+++ app/(bettacards)/TheBand/[cardId]/head.tsx
@@ -7,6 +7,7 @@ interface headProps {
 const head: FC<headProps> = ({ params: {cardId} }: headProps) => {
     return <>
         <title>The Band</title>
+        <meta httpEquiv='ScreenOrientation' content='autoRotate:disabled'/>
     </>
 }
 
