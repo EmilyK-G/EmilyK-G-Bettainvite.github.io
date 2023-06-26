9d1363d9f333630eb6cc47f9b8f5e9dfc221948b
Emily.Garcia <emilikimberly1@gmail.com>
2023-06-26 08:47:22 -0400

    autoplay bug

--- app/(bettacards)/TheBand/[cardId]/EventDate.tsx
+++ app/(bettacards)/TheBand/[cardId]/EventDate.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function EventDate() {
   return (
     <div className='h-screen w-screen flex justify-center items-end'>
-      <video className=' h-full w-full object-contain' autoPlay loop muted>
+      <video className=' h-full w-full object-contain' muted autoPlay loop>
         <source src='/media/DomsGuitarVideo.mp4' type='video/mp4'/>
       </video>
     </div>
