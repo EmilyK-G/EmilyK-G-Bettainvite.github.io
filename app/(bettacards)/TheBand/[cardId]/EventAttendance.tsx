a64e5edc2e018457e7fd493c703800b813fb2221
Emily.Garcia <emilikimberly1@gmail.com>
2023-08-18 15:00:23 -0400

    style changes

--- app/(bettacards)/TheBand/[cardId]/EventAttendance.tsx
+++ app/(bettacards)/TheBand/[cardId]/EventAttendance.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Image from 'next/image';
+import bgrPic from '../../../../public/backgrounds/curtain-bgr.jpg';
+import email from '../../../../public/icons/email-icon.png';
+import phone from '../../../../public/icons/phone-icon.png';
 
 interface eventAttendanceProps {
     openAcceptModal: any
@@ -16,17 +20,38 @@ const EventAttendance = (props: eventAttendanceProps) => {
   return (
     <div className={'flex flex-col items-center justify-around p-10 text-center h-full text-th-accent-light'}>
         <p className="text-8xl landscape:text-3xl">Contact us</p>
-        <div className='h-3/6 flex flex-col justify-evenly font-mono'>
-            <p className='text-4xl'>theband_email@gmail.com</p>
-            <p className='text-5xl'>+1 516 678 2345</p>
-            <p className='text-9xl landscape:hidden'>•</p>
+        <Image
+            src={bgrPic}
+            alt='background image'
+            className='bg-contain h-96 w-96 -z-0 rounded-full'
+          />
+        <div className='h-3/6 flex flex-col justify-evenly items-center font-mono'>
+            <div className='bg-cover h-28 w-28 rounded-full my-5'>
+                <Image
+                    src={email}
+                    onClick={()=>window.open('mailto:theband@gmail.com')}
+                    alt='email icon'
+                    className='bg-cover'
+                />
+            </div>
+            <div className='bg-cover h-24 w-24 rounded-full my-5'>
+                <Image
+                    src={phone}
+                    onClick={()=>window.open('tel:+15167257543')}
+                    alt='email icon'
+                    className='bg-cover'
+                />
+            </div>
+            {/* <p className='text-4xl'>theband_email@gmail.com</p> */}
+            {/* <p className='text-5xl'>+1 516 678 2345</p> */}
+            <p className='text-9xl'>•</p>
         </div>
-        <div className='hidden text-5xl landscape:flex space-x-2'>...</div>
-        <button
+        <div className='hidden text-5xl space-x-2'>...</div>
+        {/* <button
             className='myBtn w-full mt-10 m-2 text-5xl'
             onClick={e=>handleAccept(e)}>
             Ask something
-        </button>
+        </button> */}
     </div>
   )
 }
