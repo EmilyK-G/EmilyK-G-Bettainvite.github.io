f4b8547c4e21b2cf97a6bd6d8e736a3b37263af8
Emily.Garcia <emilikimberly1@gmail.com>
2023-08-19 16:19:25 -0400

    date change

--- app/(themes)/fallTheme/[themeId]/EventDate.tsx
+++ app/(themes)/fallTheme/[themeId]/EventDate.tsx
@@ -6,7 +6,7 @@ function EventDate() {
       <div className='flex flex-col items-center justify-evenly laptop:justify-center text-darkchocolate p-20 bg-th-primary-light h-full bg-opacity-95 text-center'>
         <h1 className='text-8xl landscape:text-4xl m-10'>Date and Time</h1>
         <div>
-          <p className='landscape:flex-1 text-8xl landscape:text-5xl m-10 landscape:m-5 leading-snug'>Sunday 11th, October 2024</p>
+          <p className='landscape:flex-1 text-8xl landscape:text-5xl m-10 landscape:m-5 leading-snug'> Saturday 7th, October 2024</p>
           <p className='text-5xl landscape:text-xl landscape:mb-5 font-mono landscape:font-sans'><span className='text-6xl landscape:text-4xl'>9AM - 5PM</span></p>
         </div>
         <p className="text-5xl landscape:text-3xl font-mono font-thin">Betalite fields, NY</p>
